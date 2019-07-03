a2c0a4c65a9b344b92d7afa731e6be7b50a09462
Barrueta <hbarruetaf97@gmail.com>
2019-07-03 12:32:37 -0500

    Ejemplo/Ejercicio de un identity

---
+++ IdentityProject/Common/CurrentUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Security;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+using Newtonsoft.Json;
+
+namespace Common
+{
+    public class CurrentUser
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public static CurrentUser Get
+        {
+            get
+            {
+                var user = HttpContext.Current.User;
+                if (user == null)
+                {
+                    return null;
+                }
+                else if(string.IsNullOrEmpty(user.Identity.GetUserId()))
+                {
+                    return null;
+                }
+                var Juser = ((ClaimsIdentity)user.Identity).FindFirst(ClaimTypes.UserData).Value;
+                return JsonConvert.DeserializeObject<CurrentUser>(Juser);
+            }
+
+        }
+    }
+}
---
+++ IdentityProject/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Model;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ApplicationUser>();
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
